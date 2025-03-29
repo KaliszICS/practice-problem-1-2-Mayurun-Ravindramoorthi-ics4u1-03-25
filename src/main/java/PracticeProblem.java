84c4391864fde55769b4af76f74d988061f8a7e9
Mayurun-Ravindramoorthi-ics4u1-03-25 <mayurun.ravindramoorthi@student.tdsb.on.ca>
2025-03-29 23:28:53 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,37 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
+		System.out.println(validIndex(new int[]{1, 3, 5}, 4));
+		System.out.println(safeConvertStringtoInt( "bruv"));
 	}
 
-	
+
+		public static boolean validIndex(int[] arr, int index) {
+			try {
+				int value = arr[index]; 
+				return true; 
+			} catch (Exception e) {
+				return false; 
+			}
+		}
+
+		public static int divide(int a, int b){
+			try{
+				return a/b;
+				
+			}
+			catch(Exception e){
+				return 0;
+			}
+		}
+	public static int safeConvertStringtoInt(String num){
+			try{
+				return Integer.parseInt(num);
+			}
+			catch(Exception e){
+				return 0;
+
+			}
+		}
 
 }
